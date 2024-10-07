a92ddce72146854965dfd690e019de9090f05107
Alibi <yoooholly71@gmail.com>
2024-10-07 13:12:58 +0200

    push

---
+++ C00/ex00/megaphone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else
+    {
+        std::string output;
+        for (int i = 1; i < argc; ++i)
+        {
+            std::string input = argv[i];
+            for (size_t j = 0; j < input.size(); ++j)
+                output += std::toupper(input[j]);
+            if (i + 1 < argc)
+                output += " ";
+        }
+        std::cout << output << std::endl;
+    }
+    return 0;
+}
---
+++ C00/ex01/PhoneBook.hpp
@@ -0,0 +1,18 @@
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+# include <iostream>
+# include <string>
+# include "Contact.hpp"
+# define MAX_CONTACTS 8
+
+class PhoneBook
+{
+    private:
+        /* data */
+    public:
+        PhoneBook(/* args */);
+        ~PhoneBook();
+};
+
+#endif
