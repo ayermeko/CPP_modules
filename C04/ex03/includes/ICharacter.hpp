1ac2282c9486b38cc0e3229d556bb68f7d65278f
Alibi <ayermeko@student.42prague.com>
2024-10-22 19:08:01 +0200

    push

--- C04/ex03/includes/ICharacter.hpp
+++ C04/ex03/includes/ICharacter.hpp
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 19:01:35 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/22 19:01:57 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/10/22 19:06:36 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 
 class ICharacter
 {
-public:
-	virtual ~ICharacter() {}
-	virtual std::string const & getName() const = 0;
-	virtual void equip(AMateria* m) = 0;
-	virtual void unequip(int idx) = 0;
-	virtual void use(int idx, ICharacter& target) = 0;
+	public:
+		virtual ~ICharacter() {}
+		virtual std::string const & getName() const = 0;
+		virtual void equip(AMateria* m) = 0;
+		virtual void unequip(int idx) = 0;
+		virtual void use(int idx, ICharacter& target) = 0;
 };
--- C04/ex03/src/AMateria.cpp
+++ C04/ex03/src/AMateria.cpp
@@ -6,7 +6,17 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 19:02:42 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/22 19:02:43 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/10/22 19:06:07 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "AMateria.hpp"
+
+AMateria::AMateria(std::string const &type) : type(type) {}
+
+AMateria::~AMateria(void) {}
+
+std::string const &AMateria::getType() const
+{
+	return (this->type);
+}
