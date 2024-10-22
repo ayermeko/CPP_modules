9d5e16a12fa15ca00d9bdcf5b6c9a1a79073e536
Alibi <ayermeko@student.42prague.com>
2024-10-22 19:03:23 +0200

    push modifiers

--- C04/ex03/includes/AMateria.hpp
+++ C04/ex03/includes/AMateria.hpp
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 17:40:31 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/22 18:15:11 by ayermeko         ###   ########.fr       */
+/*   Created: 2024/10/22 18:58:41 by ayermeko          #+#    #+#             */
+/*   Updated: 2024/10/22 19:02:27 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "ICharacter.hpp"
+
+class AMateria
+{
+	protected:
+		std::string type;
+	public:
+		AMateria(std::string const & type);
+		virtual ~AMateria(void);
+		
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
--- C04/ex03/includes/ICharacter.hpp
+++ C04/ex03/includes/ICharacter.hpp
@@ -5,8 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/22 17:40:53 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/22 18:15:18 by ayermeko         ###   ########.fr       */
+/*   Created: 2024/10/22 19:01:35 by ayermeko          #+#    #+#             */
+/*   Updated: 2024/10/22 19:01:57 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include "AMateria.hpp"
+
+class ICharacter
+{
+public:
+	virtual ~ICharacter() {}
+	virtual std::string const & getName() const = 0;
+	virtual void equip(AMateria* m) = 0;
+	virtual void unequip(int idx) = 0;
+	virtual void use(int idx, ICharacter& target) = 0;
+};
