d69c5c1d865d5d58d55c80b8ff7e810e08ec42c3
Alibi <ayermeko@student.42prague.com>
2024-10-22 21:15:07 +0200

    push

--- C04/ex03/src/Cure.cpp
+++ C04/ex03/src/Cure.cpp
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 20:45:18 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/22 20:59:30 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/10/22 21:14:27 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,20 @@ Cure::Cure(const Cure &src)
 	*this = src;
 }
 
+Cure &Cure::operator=(const Cure &src)
+{
+	(void)src;
+	return (*this);
+}
+
 Cure::~Cure(void) {}
 
 AMateria* Cure::clone() const
 {
 	return (new Cure());
+}
+
+void Cure::use(ICharacter &target)
+{
+	std::cout << "* heals " << target.getName() << "'s wounds *\n";
 }
--- C04/ex03/src/Ice.cpp
+++ C04/ex03/src/Ice.cpp
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 20:45:22 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/22 21:03:56 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/10/22 21:12:53 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,20 @@ Ice::Ice(const Ice &src)
 	*this = src;
 }
 
+Ice &Ice::operator=(const Ice &src)
+{
+	(void)src;
+	return(*this);
+}
+
 Ice::~Ice(void) {}
 
 AMateria *Ice::clone() const
 {
 	return (new Ice());
+}
+
+void Ice::use(ICharacter &target)
+{
+	std::cout << "* shoots an ice bolt at " << target.getName() << std::endl;
 }
