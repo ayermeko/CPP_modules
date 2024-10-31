42a04744333c72b502ab591bcda0db90d700ecfa
Alibi <yoooholly71@gmail.com>
2024-10-31 14:18:25 +0100

    file modified

--- CPP05/ex00/Bureaucrat.hpp
+++ CPP05/ex00/Bureaucrat.hpp
@@ -3,11 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
+/*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:53:19 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/10/31 10:53:24 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/10/31 14:16:44 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include <iostream>
+#include <string>
+
+class Bureaucrat
+{
+    private:
+        const std::string   name;
+        const int           grade;
+        static const int    MAX_VAL = 150;
+        static const int    MIN_VAL = 1;
+    public:
+        Bureaucrat(void);
+        Bureaucrat &operator=(const Bureaucrat &src);
+        Bureaucrat(const Bureaucrat &src);
+        ~Bureaucrat(void);
+
+        const std::string &getName(void) const;
+        const int &getGrade(void) const;
+
+        void increment(void);
+        void decrement(void);
+};
