ace806c0c30fa60160b8a3d74577c35122d5ab28
Alibi <ayermeko@student.42prague.com>
2024-11-04 16:10:42 +0100

    push

--- CPP05/ex02/includes/PresidentialPardonForm.hpp
+++ CPP05/ex02/includes/PresidentialPardonForm.hpp
@@ -3,11 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   PresidentialPardonForm.hpp                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayermeko <ayermeko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:27:13 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/11/02 23:27:38 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:09:58 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
+
+#include "AForm.hpp"
+
+class PresidentialPardonForm : public AForm
+{
+	private:
+		PresidentialPardonForm(void);
+		void executeConcrete() const;
+
+	public:
+		PresidentialPardonForm(const std::string &target);
+		PresidentialPardonForm(const PresidentialPardonForm &src);
+		PresidentialPardonForm &operator=(const PresidentialPardonForm &src);
+		~PresidentialPardonForm(void);
+};
--- CPP05/ex02/includes/ShrubberyCreationFrom.hpp
+++ CPP05/ex02/includes/ShrubberyCreationFrom.hpp
@@ -6,10 +6,25 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 23:27:18 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/11/03 12:13:27 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/11/04 16:09:09 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include "AForm.hpp"
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
+
+class ShrubberyCreationFrom : public AForm
+{
+	private:
+		ShrubberyCreationFrom(void);
+		void executeConcrete() const;
+	public:
+		ShrubberyCreationFrom(const std::string &target);
+		ShrubberyCreationFrom(const ShrubberyCreationFrom &src);
+		ShrubberyCreationFrom &operator=(const ShrubberyCreationFrom &src);
+		~ShrubberyCreationFrom(void);
+};
