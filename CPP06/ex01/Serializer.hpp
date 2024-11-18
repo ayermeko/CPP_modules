2c8d3e6bbdfe13cf5568e3cc13d919583fea9062
Alibi <ayermeko@student.42prague.com>
2024-11-18 14:52:46 +0100

    push

--- CPP06/ex01/Serializer.hpp
+++ CPP06/ex01/Serializer.hpp
@@ -6,13 +6,15 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 13:28:21 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/11/18 14:13:08 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/11/18 14:52:08 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <iostream>
+#include <string>
+#include <stdint.h>
 #include "Data.hpp"
 
 class Serializer
