57960b225ab0bb8b948c2fd985cab942d4ae9048
Alibi <ayermeko@student.42prague.com>
2024-11-27 14:55:55 +0100

    push

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: ayermeko <ayermeko@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 11:55:04 by ayermeko          #+#    #+#             */
-/*   Updated: 2024/11/27 11:56:44 by ayermeko         ###   ########.fr       */
+/*   Updated: 2024/11/27 14:50:28 by ayermeko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,9 @@
 #include <iostream>
 #include <string>
 
+template<typename T_array, typename T_function>
+void iter(T_array *array, size_t length, T_function function)
+{
+	for (size_t i = 0; i < length; i++)
+		function(array[i]);
+}
